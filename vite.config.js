35fe96d9355701c6df775ab52c8eef9796cdd571
alfonso <alfonsohernandezrincon4@gmail.com>
2024-11-14 16:13:27 -0500

    dist

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        outDir: 'dist', // Especifica que la salida debe ir a la carpeta 'dist'
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
